a69b20b0bfafb0a2e45c9f011fd4c2092452e98b
Barbara Pakula <barbara.pakula@hotmail.com>
2021-06-12 10:21:58 +0200

    Add dark layuot

--- server.js
+++ server.js
@@ -10,39 +10,32 @@ app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-// app.use((req, res, next) => {
-//   res.show = (name) => {
-//     res.sendFile(path.join(__dirname, `/views/${name}`));
-//   };
-//   next();
-// });
-
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/', (req, res) => {
-  res.render('index', {layout : false});
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', {layout: false});
+  res.render('about', {layout: 'dark'});
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact', {layout: false});
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.render('info', {layout: false});
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.render('history', {layout: false});
+  res.render('history');
 });
 
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
+  res.status(404).sendFile(path.join(__dirname,'public/404.png'));
 })
 
 app.listen(8000, () => {
